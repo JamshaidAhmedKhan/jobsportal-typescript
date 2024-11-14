6f89d6da8e28c5cf164dbd9a3757d8d89dc959e6
JamshaidAhmedKhan <official.jimmi666@gmail.com>
2024-11-15 01:00:12 +0500

    initial-commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // Add your HTML files here
+    "./src/**/*.{js,jsx,ts,tsx}", // Add your JS/TS files (for React or Vue)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
